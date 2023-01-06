27a12c4331e99bec22c1af8b46825a6c8997f7cf
Fryderyk Jellinek <fryderyk.jellinek@gmail.com>
2023-01-06 23:28:18 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+const getProducts = async () => {
+    // const response = await fetch('https://api.escuelajs.co/api/v1/products/'); duzo elementów
+    const response = await fetch('https://api.storerestapi.com/products');
+    const products = await response.json();
+
+
+    return products;
+}
+
+
+document.addEventListener('DOMContentLoaded', async () => {
+    let products = [];
+
+    try {
+        products = await getProducts();
+    } catch (e) {
+        console.log(e)
+    }
+    
+    console.log(products);
+})
+
+//  do tego napisac funckje, ktore bedzie obejmowac tez li !
+const navMobile = document.querySelector('.nav_mobile');
+const burgerBtn = document.querySelector('.burger_btn').addEventListener('click', () => {
+    navMobile.classList.toggle('show');
+})
