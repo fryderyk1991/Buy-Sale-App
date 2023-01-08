e7b317dbb33554f4ae5d66e82217de4066a79979
Fryderyk Jellinek <fryderyk.jellinek@gmail.com>
2023-01-08 17:13:11 +0100

    nav mobile & nav desktop created

--- script.js
+++ script.js
@@ -22,6 +22,8 @@ document.addEventListener('DOMContentLoaded', async () => {
 
 //  do tego napisac funckje, ktore bedzie obejmowac tez li !
 const navMobile = document.querySelector('.nav_mobile');
+const burgerLines = document.querySelectorAll('.burger_btn > div');
 const burgerBtn = document.querySelector('.burger_btn').addEventListener('click', () => {
     navMobile.classList.toggle('show');
+    burgerLines.forEach((line) => line.classList.toggle('show'));
 })
