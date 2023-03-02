a8274c3d4d748c197e0d94ffe465b743276840aa
Fryderyk Jellinek <fryderyk.jellinek@gmail.com>
2023-03-02 21:31:44 +0100

    select quantinity and price function

--- js/cart.js
+++ js/cart.js
@@ -31,7 +31,7 @@ const showCartProducts = async () => {
                 </div>
                 <p class="cart_product--desc">${product.title}</p>
                 <div class="cart_product-price--quantinity">
-                <p>${product.price}$</p>
+                <p data-price=${product.price} data-price-id=${product.id}>${product.price}$</p>
                 <select class="cart_select" data-id=${product.id}>
                 <option value="1">1</option>
                 <option value="2">2</option>
@@ -57,12 +57,13 @@ const showCartProducts = async () => {
     }
 
     const cartProductDetails = document.querySelectorAll('.cart_product--image > img');
+    
     const showProductDetails = (e) => {
         let productImage = e.target.getAttribute("data-image");
         let url = `/product-details.html?id=${productImage}`;
         window.location.href = url;
-
     }
+    
 
     cartProductDetails.forEach((item) => item.addEventListener('click', showProductDetails));
     
@@ -70,8 +71,69 @@ const showCartProducts = async () => {
         btn.addEventListener('click', deleteProduct);
     });
 
+    const pricesObjArray = [];
+
+    const productPrices = document.querySelectorAll('[data-price][data-price-id]').forEach((item) => {
+        pricesObjArray.push({itemId : item.getAttribute('data-price-id'), itemPrice : item.getAttribute('data-price')});
+    });
+
+    // const pricesToNum = pricesObjArray.map((item) => { return item.itemId = +item.itemId, item.itemPrice =+item.itemPrice });
     
-}
+    console.log(pricesObjArray)
+    // nodlist z selectami 
+    const quantinitySelect = document.querySelectorAll('.cart_select');
+
+    const selectQuantinity = (e) => {
+        let option = e.target.value;
+        let selectId = e.target.getAttribute("data-id");
+        let currentSelect = e.target.previousElementSibling;
+        const matchIdSelect = pricesObjArray.filter((item) => {return item.itemId === selectId});
+        if (matchIdSelect) {
+            const currentPrice = matchIdSelect.map((item) => {return item.itemPrice * option});
+            currentSelect.innerHTML = `${currentPrice}$`
+         }
+    }
+
+    quantinitySelect.forEach((option) => {option.addEventListener('click', selectQuantinity)});
+
+ 
+    /// ----->
+    // cena za cały koszyk 
+    // const totalPriceOfCartItems = document.querySelector(".total_price > p");
+
+    // // tablica z cenami producktów 
+    // const pricesArray = [];
+
+    // // nodelist do którego dodajemy value data_price!
+    // const productPrices = document.querySelectorAll('[data-price]').forEach((price) => {
+    //     pricesArray.push(price.getAttribute("data-price"));
+    // });
+
+
+    // // tablica która konwertuje stringi na numery 
+    // const pricesToNum = pricesArray.map((price) => {return price = +price});
+
+    // const sumPrices = () => {
+    //     let sum = 0;
+    //     pricesToNum.forEach((item) => {return sum+=item});
+    //     totalPriceOfCartItems.innerHTML = `${sum} $`;
+    // }
+    // sumPrices()
+
+    // // nodlist z selectami 
+    // const quantinitySelect = document.querySelectorAll('.cart_select');
+
+    // const selectQuantinity = (e) => {
+    //     let option = e.target.value;
+    //     let selectId = e.target.getAttribute("data-id");
+
+
+    //     // console.log(option, selectId, priceId)
+    //     console.log(pricesArray)
+    // }
+
+    // quantinitySelect.forEach((option) => {option.addEventListener('click', selectQuantinity)});
+ }
 
 
 
