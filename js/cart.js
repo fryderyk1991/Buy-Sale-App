50b72d27edbd67a1de1dc1dbfa5c03bfe25164ff
Fryderyk Jellinek <fryderyk.jellinek@gmail.com>
2023-04-09 13:05:55 +0200

    update cart component

--- js/cart.js
+++ js/cart.js
@@ -1,30 +1,28 @@
 import { changeValueOfCounter } from "./utils.js";
 
-
 const getCartProducts = async () => {
-    const response = await fetch('https://fakestoreapi.com/products');
-    const products = await response.json();
+  const response = await fetch("https://fakestoreapi.com/products");
+  const products = await response.json();
 
-
-    return products;
+  return products;
 };
 
 const showCartProducts = async () => {
-    
-   const cartContainer = document.querySelector('.cart_container');
-   let products = [];
-   
-   try {
-        products = await getCartProducts();
-   } catch (e) {
-    console.log(e)
-   }
+  const cartContainer = document.querySelector(".cart_container");
+  let products = [];
 
-    let cartProductsID = (JSON.parse(localStorage.getItem('cartArray'))) || [];
+  try {
+    products = await getCartProducts();
+  } catch (e) {
+    console.log(e);
+  }
 
-    
-        const currentCartProducts = products.filter(product => cartProductsID.some(item => item == product.id)).map((product) => {
-            cartContainer.innerHTML += `
+  let cartProductsID = JSON.parse(localStorage.getItem("cartArray")) || [];
+
+  const currentCartProducts = products
+    .filter((product) => cartProductsID.some((item) => item == product.id))
+    .map((product) => {
+      cartContainer.innerHTML += `
             <div class="cart_container--product">
                 <div class="cart_product--image" >
                 <img src=${product.image} data-image=${product.id}>
@@ -42,100 +40,77 @@ const showCartProducts = async () => {
                 <i class="fa-solid fa-trash" data-delete=${product.id}></i>
                 </div>
             <div>`;
-        })
-
-    const deleteProduct = (e) => {
-        let deleteIconId = e.target.getAttribute("data-delete");
-        let deleteCurrentProduct = cartProductsID.filter((product) => { return product !== deleteIconId });
-        if (deleteCurrentProduct) {
-            cartProductsID = deleteCurrentProduct;
-            localStorage.setItem('cartArray', JSON.stringify(deleteCurrentProduct));
-            cartContainer.innerHTML = '';
-            showCartProducts();
-            changeValueOfCounter();
-        }
-    }
-
-    const cartProductDetails = document.querySelectorAll('.cart_product--image > img');
-    
-    const showProductDetails = (e) => {
-        let productImage = e.target.getAttribute("data-image");
-        let url = `/product-details.html?id=${productImage}`;
-        window.location.href = url;
-    }
-    
-
-    cartProductDetails.forEach((item) => item.addEventListener('click', showProductDetails));
-    
-    const deleteBtn = document.querySelectorAll('[data-delete]').forEach((btn) => {
-        btn.addEventListener('click', deleteProduct);
     });
 
-    const pricesObjArray = [];
+  const deleteProduct = (e) => {
+    let deleteIconId = e.target.getAttribute("data-delete");
+    let deleteCurrentProduct = cartProductsID.filter((product) => {
+      return product !== deleteIconId;
+    });
+    if (deleteCurrentProduct) {
+      cartProductsID = deleteCurrentProduct;
+      localStorage.setItem("cartArray", JSON.stringify(deleteCurrentProduct));
+      cartContainer.innerHTML = "";
+      showCartProducts();
+      changeValueOfCounter();
+    }
+  };
 
-    const productPrices = document.querySelectorAll('[data-price][data-price-id]').forEach((item) => {
-        pricesObjArray.push({itemId : item.getAttribute('data-price-id'), itemPrice : item.getAttribute('data-price')});
+  const cartProductDetails = document.querySelectorAll(
+    ".cart_product--image > img"
+  );
+
+  const showProductDetails = (e) => {
+    let productImage = e.target.getAttribute("data-image");
+    let url = `/product-details.html?id=${productImage}`;
+    window.location.href = url;
+  };
+
+  cartProductDetails.forEach((item) =>
+    item.addEventListener("click", showProductDetails)
+  );
+
+  const deleteBtn = document
+    .querySelectorAll("[data-delete]")
+    .forEach((btn) => {
+      btn.addEventListener("click", deleteProduct);
     });
 
-    // const pricesToNum = pricesObjArray.map((item) => { return item.itemId = +item.itemId, item.itemPrice =+item.itemPrice });
-    
-    console.log(pricesObjArray)
-    // nodlist z selectami 
-    const quantinitySelect = document.querySelectorAll('.cart_select');
+  let pricesAndIdObjArray = [];
 
-    const selectQuantinity = (e) => {
-        let option = e.target.value;
-        let selectId = e.target.getAttribute("data-id");
-        let currentSelect = e.target.previousElementSibling;
-        const matchIdSelect = pricesObjArray.filter((item) => {return item.itemId === selectId});
-        if (matchIdSelect) {
-            const currentPrice = matchIdSelect.map((item) => {return item.itemPrice * option});
-            currentSelect.innerHTML = `${currentPrice}$`
-         }
-    }
+  const productPricesAndId = document
+    .querySelectorAll("[data-price][data-price-id]")
+    .forEach((item) => {
+      pricesAndIdObjArray.push({
+        itemId: item.getAttribute("data-price-id"),
+        itemPrice: +item.getAttribute("data-price"),
+      });
+    });
 
-    quantinitySelect.forEach((option) => {option.addEventListener('click', selectQuantinity)});
+  const quantinitySelect = document.querySelectorAll(".cart_select");
 
- 
-    /// ----->
-    // cena za cały koszyk 
-    // const totalPriceOfCartItems = document.querySelector(".total_price > p");
+  const matchSelectWithId = [];
 
-    // // tablica z cenami producktów 
-    // const pricesArray = [];
+  const selectQuantinity = (e) => {
+    let quantinity = e.target.value;
+    let selectId = e.target.getAttribute("data-id");
 
-    // // nodelist do którego dodajemy value data_price!
-    // const productPrices = document.querySelectorAll('[data-price]').forEach((price) => {
-    //     pricesArray.push(price.getAttribute("data-price"));
-    // });
+    const matchSelectWithId = pricesAndIdObjArray.filter((item) => item.itemId == selectId);
+    const currentPriceAfterSelect = matchSelectWithId.map((item) => item.itemPrice * quantinity);
+    let newPriceAfterSelect = e.target.previousElementSibling;
+    newPriceAfterSelect.innerHTML = `${currentPriceAfterSelect}$`;
+  };
+  quantinitySelect.forEach((option) => {
+    option.addEventListener("click", selectQuantinity);
+  });
 
-
-    // // tablica która konwertuje stringi na numery 
-    // const pricesToNum = pricesArray.map((price) => {return price = +price});
-
-    // const sumPrices = () => {
-    //     let sum = 0;
-    //     pricesToNum.forEach((item) => {return sum+=item});
-    //     totalPriceOfCartItems.innerHTML = `${sum} $`;
-    // }
-    // sumPrices()
-
-    // // nodlist z selectami 
-    // const quantinitySelect = document.querySelectorAll('.cart_select');
-
-    // const selectQuantinity = (e) => {
-    //     let option = e.target.value;
-    //     let selectId = e.target.getAttribute("data-id");
-
-
-    //     // console.log(option, selectId, priceId)
-    //     console.log(pricesArray)
-    // }
-
-    // quantinitySelect.forEach((option) => {option.addEventListener('click', selectQuantinity)});
- }
-
-
-
-document.addEventListener('DOMContentLoaded', showCartProducts);
-
+  const totalPriceOfCartItems = document.querySelector(".total_price > p");
+  let sum = 0;
+  const sumPrices = () => {
+    pricesAndIdObjArray.forEach((item) => {return sum+= item.itemPrice});
+    totalPriceOfCartItems.innerHTML = `${sum}$`;
+    }  
+  sumPrices();
+   
+};
+document.addEventListener("DOMContentLoaded", showCartProducts);
